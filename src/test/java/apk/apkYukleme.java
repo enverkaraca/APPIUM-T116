65e66eec7772cea085ab7f6d21e583f694e98467
enver <enverkaraca.wise@gmail.com>
2023-08-22 23:33:23 +0200

    v2enver

---
+++ src/test/java/apk/apkYukleme.java
@@ -0,0 +1,22 @@
+package apk;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+
+public class apkYukleme {
+    AndroidDriver<AndroidElement> driver;
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 2");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+    }
+}
